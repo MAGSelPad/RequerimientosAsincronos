9dd9aa1a5243bdf8461d7e393971e754c4c7ed41
MAGSelPad <150753146+MAGSelPad@users.noreply.github.com>
2026-06-17 19:56:29 +0000

    Implementación de función getSalesCoffee()

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let getSalesCoffee = async () => {
+
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Coffee/Coffe_sales.xml");
+
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        let text = await response.text()
+
+        const parser = new DOMParser();
+        const data = parser.parseFromString(text, "application/xml");
+
+        return {
+            success: true,
+            body: data
+        };
+
+    } catch (error) {
+
+        return {
+            success: false,
+            body: error.message
+        };
+
+    }
+}
